1d72356ecb2c91f5ecb4d707d34d9ccb308d97cf
sanix sandel <sanicksikani@gmail.com>
2020-06-25 12:51:28 +0300

    retrieve/saving work perfectly

--- backend/backend/notes/views.py
+++ backend/backend/notes/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from . import models
 from . import serializers
 
-class NoteList(generics.ListAPIView):
+class NoteList(generics.ListCreateAPIView):
     queryset=models.Note.objects.all()
     serializer_class=serializers.NoteSerializer
 
